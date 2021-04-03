820a4569f5da4be41b85e4784d29799036458610
Koji Wakamiya <koji.wakamiya@gmail.com>
2021-04-03 13:32:46 +0900

    Simplify

--- lib/src/breakpoints_media_query_ext.dart
+++ lib/src/breakpoints_media_query_ext.dart
@@ -2,7 +2,6 @@ import 'package:flutter/widgets.dart';
 
 import 'breakpoint.dart';
 import 'breakpoints_enum.dart';
-import 'breakpoints_enum_ext.dart';
 
 /// [MediaQueryData] extension to get [BreakpointRange] and [BreakpointWindow].
 extension BreakpointsMediaQueryExt on MediaQueryData {
@@ -10,18 +9,17 @@ extension BreakpointsMediaQueryExt on MediaQueryData {
   Breakpoint get breakpoint => Breakpoint.fromWidth(width: size.width);
 
   /// Get [BreakpointRange] from [MediaQueryData].
-  BreakpointRange get breakpointRange =>
-      BreakpointRangeExt.range(width: size.width);
+  BreakpointRange get breakpointRange => breakpoint.range;
 
   /// Get [BreakpointWindow] from [MediaQueryData].
-  BreakpointWindow get breakpointWindow => breakpointRange.window;
+  BreakpointWindow get breakpointWindow => breakpoint.window;
 
   /// Get the [int] of columns from [MediaQueryData].
-  int get breakpointColumns => breakpointRange.columns;
+  int get breakpointColumns => breakpoint.columns;
 
   /// Get the [int] of margins from [MediaQueryData].
-  int get breakpointMargins => breakpointRange.margins;
+  int get breakpointMargins => breakpoint.margins;
 
   /// Get the [int] of gutters from [MediaQueryData].
-  int get breakpointGutters => breakpointRange.gutters;
+  int get breakpointGutters => breakpoint.gutters;
 }
