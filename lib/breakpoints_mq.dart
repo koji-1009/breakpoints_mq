85b16a1434c7ab4c4069155f1226d716bdeffa93
Koji Wakamiya <koji.wakamiya@gmail.com>
2022-03-19 22:14:55 +0900

    Add BreakpointWidget

--- lib/breakpoints_mq.dart
+++ lib/breakpoints_mq.dart
@@ -1,4 +1,5 @@
 export 'src/breakpoint.dart';
+export 'src/breakpoint_widget.dart';
 export 'src/breakpoints_enum.dart';
 export 'src/breakpoints_enum_ext.dart';
 export 'src/breakpoints_media_query_ext.dart';
---
+++ lib/src/breakpoint_widget.dart
@@ -0,0 +1,24 @@
+import 'package:breakpoints_mq/src/breakpoints_box_constraints_ext.dart';
+import 'package:flutter/material.dart';
+
+/// A widget with padding added, depending on the constraints the widget has.
+class BreakpointWidget extends StatelessWidget {
+  const BreakpointWidget({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) => Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: constraints.breakpointMargin,
+        ),
+        child: child,
+      ),
+    );
+  }
+}
