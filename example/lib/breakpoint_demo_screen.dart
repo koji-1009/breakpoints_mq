d125df8b2a161636440b5350c5dedec5bb3187b1
Koji Wakamiya <koji.wakamiya@gmail.com>
2023-04-05 23:12:19 +0900

    Upgrade project

--- example/lib/breakpoint_demo_screen.dart
+++ example/lib/breakpoint_demo_screen.dart
@@ -2,7 +2,7 @@ import 'package:breakpoints_mq/breakpoints_mq.dart';
 import 'package:flutter/material.dart';
 
 class BreakpointDemoScreen extends StatelessWidget {
-  const BreakpointDemoScreen({Key? key}) : super(key: key);
+  const BreakpointDemoScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
--- example/lib/breakpoint_widget_demo_screen.dart
+++ example/lib/breakpoint_widget_demo_screen.dart
@@ -2,7 +2,7 @@ import 'package:breakpoints_mq/breakpoints_mq.dart';
 import 'package:flutter/material.dart';
 
 class BreakpointWidgetDemoScreen extends StatelessWidget {
-  const BreakpointWidgetDemoScreen({Key? key}) : super(key: key);
+  const BreakpointWidgetDemoScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
